c1b6a6d8816f08b0306e0a1f1ab4d5c031cf13be
Elvix10 <brunel55@hotmail.com>
2023-07-20 16:59:33 -0100

    swal fire added

--- share-recipies-api/app/Controllers/Http/AuthController.ts
+++ share-recipies-api/app/Controllers/Http/AuthController.ts
@@ -1,20 +1,21 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
-
+import User from "App/Models/User";
 
 export default class AuthController {
   async login({ request, auth, response }: HttpContextContract) {
     const username = request.input("username");
     const password = request.input("password");
 
-    console.log(username);
-    
+    let user = await User.query().where("username", username).firstOrFail();
+
+    console.log("user", user);
 
     try {
       const token = await auth.use("api").attempt(username, password);
-      return token;
+      return { token: token, user: { username: user.username, id: user.id } };
     } catch (err) {
       console.log(err);
-      
+
       return response.unauthorized("Invalid credentials");
     }
   }
